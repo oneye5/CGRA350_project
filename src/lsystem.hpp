ae91f75b9c21479869c8d9e913de834ee66548df
Sofía Meza <mezassofi@ecs.vuw.ac.nz>
2025-10-16 09:06:46 +1300

    Remove lsystem gui stuff
    doesn't work too well with the new things

--- src/lsystem.hpp
+++ src/lsystem.hpp
@@ -8,25 +8,4 @@ namespace lsystem {
 	typedef std::unordered_map<char, std::string> ruleset;
 	ruleset parse_rules(std::istream& rules);
 	std::string iterate(const std::string& seed, const ruleset& rules, int count = 1);
-
-	namespace gui {
-		static const char* available_rule_names[] = {
-			"",
-			"Basic Tree",
-			"Debug",
-		};
-		static const char* available_rule_paths[] = {
-			"",
-			CGRA_SRCDIR "/res/rules/basic_tree.txt",
-			CGRA_SRCDIR "/res/rules/debug.txt",
-		};
-		struct Data {
-			std::string seed;
-			std::optional<ruleset> rules;
-			int interations;
-			bool regrow;
-		};
-		void rules_window(struct Data& data);
-		void growth_window(struct Data& data);
-	}
 }
